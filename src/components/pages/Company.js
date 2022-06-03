4c34f51817ab47cfb2bdee33af3bbbbcea82f1ae
Luis Felipe Saldanha <103341404+luiszsaldanha@users.noreply.github.com>
2022-06-03 18:39:41 -0300

    Costs

---
+++ src/components/pages/Company.js
@@ -0,0 +1,7 @@
+function Comapany(){
+    return(
+        <h1>Company</h1>
+    )
+}
+
+export default Comapany
---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,17 @@
+import ProjectForm from '../project/ProjectForm';
+
+import styles from './NewProject.module.css';
+
+
+
+function NewProject(){
+    return(
+            <div className={styles.newproject_container} >
+            <h1>Criar Projeto</h1>
+            <p>Crie seu Projeto para depois adicionar os serviços</p>
+            <ProjectForm/>
+        </div>
+    )
+}
+
+export default NewProject
