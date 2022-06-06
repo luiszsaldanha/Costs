af162b5f7f29abc225159076f6dab9421358ec21
Luis Felipe Saldanha <103341404+luiszsaldanha@users.noreply.github.com>
2022-06-06 14:29:05 -0300

    update

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,3 +1,5 @@
+import {useNavigate} from 'react-router-dom'
+
 import ProjectForm from '../project/ProjectForm';
 
 import styles from './NewProject.module.css';
@@ -5,11 +7,38 @@ import styles from './NewProject.module.css';
 
 
 function NewProject(){
+    
+    const history = useNavigate()
+
+     function creatPost(project){
+    
+     //initialize cost and services
+
+    project.costs = 0
+    project.services = []
+   
+    fetch("http://localhost:5000/projects", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(project),
+    }) 
+    .then((resp) => resp.json())
+    .then(data => {
+        console.log(data)
+        //redirect 
+        history('/projects', {mensage: 'Projeto criado com sucesso!'}) 
+       
+    }
+    ).catch(err => console.log(err))
+    
+}
     return(
             <div className={styles.newproject_container} >
             <h1>Criar Projeto</h1>
             <p>Crie seu Projeto para depois adicionar os serviços</p>
-            <ProjectForm/>
+            <ProjectForm handleSubmit={creatPost} btnText='Criar Projeto'/>
         </div>
     )
 }
